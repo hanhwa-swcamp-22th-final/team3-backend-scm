2953e2dec28e274e5a14758b814067471a8a24cf
rlatlsdn98 <rlatlsdn98@naver.com>
2026-04-16 12:18:22 +0900

    ELS, prometheus, grafana 셋업

--- src/main/java/com/ohgiraffers/team3backendscm/config/SecurityConfig.java
+++ src/main/java/com/ohgiraffers/team3backendscm/config/SecurityConfig.java
@@ -56,6 +56,7 @@ public class SecurityConfig {
                              .accessDeniedHandler(restAccessDeniedHandler))
             .authorizeHttpRequests(auth ->
                     auth.requestMatchers("/error").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
                         .requestMatchers(
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
